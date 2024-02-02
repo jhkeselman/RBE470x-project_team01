d6ec19d5b02869139de54d5f4fa752e9ae538317
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2024-02-02 16:14:49 -0500

    Started a class for ai character

---
+++ team01/aicharacter.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.insert(0, '../bomberman')
+# Import necessary stuff
+from entity import CharacterEntity
+from colorama import Fore, Back
+
+class TestCharacter(CharacterEntity):
+
+    def do(self, wrld):
+        # Your code here
+        pass
