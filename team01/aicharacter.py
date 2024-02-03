edf5aab6fde4b082a1fcba385796039f7640f0d7
joshkeselman <68922866+joshkeselman@users.noreply.github.com>
2024-02-03 15:58:21 -0500

    Starting v1 code Co-authored-by: Cameron Wian <MAC6156@users.noreply.github.com> Co-authored-by: Markwick, Sam <spmarkwick@wpi.edu>

--- team01/aicharacter.py
+++ team01/aicharacter.py
@@ -6,36 +6,80 @@ sys.path.insert(0, '../bomberman')
 from entity import CharacterEntity
 from colorama import Fore, Back
 
+# class State(Enum):
+#     SEARCH = 0
+#     EXIT = 1
+
 class AICharacter(CharacterEntity):
+    
+    # curState = 0
+    # name, avatar, x, y = "", "", 0, 0
+    # def __init__(self, name, avatar, x, y):
+    #     self.name = name
+    #     self.avatar = avatar
+    #     self.x = x
+    #     self.y = y
+    #     self.curState = State.SEARCH
 
     def do(self, wrld):
-        class State(Enum):
-            SEARCH = 0
-            EXIT = 1
-        curState = State.SEARCH
+        # if self.curState == self.State.SEARCH:
+        exitY, exitX = self.findExit(wrld)
+        path = self.astar(wrld, [self.y, self.x], [exitY, exitX])
+        # if len(path) != 0:
+        #     self.curState = self.State.EXIT
+        # # # # # # # # # # #     
+        # if self.curState == self.State.EXIT:
+        while path:
+            nextPoint = path.pop(0)
+            dx, dy = nextPoint[1] - self.x, nextPoint[0] - self.y
+            self.move(dx,dy)
+            path = self.astar(wrld, (self.y, self.x), (exitY, exitX))
+        
+        
+    def astar(self, wrld, start, goal):
+        path = []
+        found = False
+        rows, cols = wrld.height(), wrld.width()
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
+        costs = [[float('inf') for _ in range(cols)] for _ in range(rows)]
+        parent = [[None for _ in range(cols)] for _ in range(rows)]
+        parent[start[0]][start[1]] = (-1,-1)
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        queue = [(0 + self.heuristic(start, goal), 0, start[0], start[1])]
+        costs[start[0]][start[1]] = 0
 
-        if curState == State.SEARCH:
-            exitRow, exitCol = 10, 10
-            for row in range(wrld.height()):
-                for col in range(wrld.width()):
-                    if wrld.exit_at(col, row):
-                        exitRow, exitCol = row, col
-            print(exitRow, exitCol)
-            dx, dy = 0, 0
-            bomb = False
-            for c in input("How would you like to move (w=up,a=left,s=down,d=right,b=bomb)? "):
-                if 'w' == c:
-                    dy -= 1
-                if 'a' == c:
-                    dx -= 1
-                if 's' == c:
-                    dy += 1
-                if 'd' == c:
-                    dx += 1
-                if 'b' == c:
-                    bomb = True
-            self.move(dx, dy)
-        if curState == State.EXIT:
-            # Move to exit
-            pass
-        pass
+        while queue:
+            _, cost, curRow, curCol = heappop(queue)
+            
+            if [curRow, curCol] == goal:
+                found = True
+                break
+
+            for direction in directions:
+                newRow, newCol = curRow + direction[0], curCol + direction[1]
+                if 0 <= newRow < rows and 0 <= newCol < cols and not visited[newRow][newCol] and not wrld.wall_at(newCol, newRow):
+                    newCost = cost + 1
+                    if newCost < costs[newRow][newCol]:
+                        costs[newRow][newCol] = newCost
+                        prio = newCost + self.heuristic((newRow, newCol), goal)
+                        parent[newRow][newCol] = (curRow, curCol)
+                        visited[newRow][newCol] = True
+                        heappush(queue, (prio, newCost, newRow, newCol))
+
+        if found:
+            curR, curC = goal[0], goal[1]
+            while curR != -1 and curC != -1:
+                path.insert(0,[curR, curC])
+                curR, curC = parent[curR][curC][0], parent[curR][curC][1]
+        return path
+    
+    def heuristic(self,p1, p2):
+        return max(abs(p2[0]-p1[0]),abs(p2[1]-p1[1]))
+        
+    def findExit(self,wrld):
+        exitY, exitX = 0, 0
+        for row in range(wrld.height()):
+            for col in range(wrld.width()):
+                if wrld.exit_at(col, row):
+                    exitY, exitX = row, col
+        return exitY, exitX
